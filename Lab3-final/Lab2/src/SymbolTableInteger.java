60702df72d496d496b3e092d5990b240515e5e47
Bianca Szekely <127339798+biancaszekely32@users.noreply.github.com>
2023-11-07 18:26:41 +0200

    Lab3 done

---
+++ Lab3-final/Lab2/src/SymbolTableInteger.java
@@ -0,0 +1,26 @@
+
+public class SymbolTableInteger {
+
+    private HashTable<Integer> hashTable;
+
+    public SymbolTableInteger(Integer size) {
+        hashTable = new HashTable<>(size);
+    }
+
+    public boolean addSymbolInteger(Integer symbol) {
+        if (hashTable.put(symbol))
+            return hashTable.put(symbol);
+        return false;
+    }
+
+    public Integer getSymbolPosition(Integer symbol) {
+        Pair position = hashTable.findPosOfKey(symbol);
+        if (position != null) {
+            return (Integer) position.getPos();
+        }
+        return null;
+    }
+
+    public String displayTable() {
+        return hashTable.displayTable();
+    }}
